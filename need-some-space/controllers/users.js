d0ef32e5de59028b83db34303cbba76255ebfd5c
Charles Ata <charlesata@hotmail.ca>
2021-08-11 22:08:08 -0400

    Update styling

--- need-some-space/controllers/users.js
+++ need-some-space/controllers/users.js
@@ -4,6 +4,7 @@ let Article = require('../models/article');
 async function show(req, res){
     let user = await User.findById(req.user._id);
     let articles = await Article.where('_id').in(user.articlesCreated);
+    articles.sort((a, b) => b.createdAt - a.createdAt);
     res.render('users/show', { user, articles })
 }
 
